d09209eb760aa70959e59f9d8facaebf4665c04c
FERNANDO ISRAEL LOPEZ BURBANO <filopezb@utn.edu.ec>
2025-12-02 07:39:39 -0500

    nwe 1

--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -17,7 +17,7 @@ namespace WebApplication3
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            //if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
